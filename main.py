8cf7824adb23085210881cb5cd21c99a4892dd46
egor <egvlsavchenko@gmail.com>
2020-03-21 11:35:50 +0300

    Реализовали базовую логику игры

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+# Вызывается tkinter и messagebox для дальнейшего использования
 import tkinter as tk
 from tkinter import messagebox
 import area_data
@@ -5,13 +6,15 @@ root = tk.Tk()
 
 min_game_area_size = 4
 max_game_area_size = 10
+game_area_size = 0
 
 restart_button = None
 game_area_data = []
 buttons_list = []
 
+# Функция для запуска игры с полем ввода размера и кнопкой для запуска
 def init_game(start_button, input_element):
-    global game_area_data
+    global game_area_data, game_area_size
 
     game_area_size = input_element.get()
 
@@ -30,6 +33,7 @@ def init_game(start_button, input_element):
 
     start_game(game_area_size)
 
+# Указываются минимальные и максимальные размеры игрового поля
 def get_correct_size(n):
     if n < min_game_area_size:
         return min_game_area_size
@@ -37,10 +41,11 @@ def get_correct_size(n):
         return max_game_area_size
     return n
 
-
+# Функция по перезапуску игры
 def restart_game():
     global restart_buttond
 
+    # После нажатия кнопки перезапуска, игровое поле и кнопка пропадют
     if restart_button:
         restart_button.destroy()
     for button in buttons_list:
@@ -50,31 +55,39 @@ def restart_game():
 
     start()
 
+# Проверка выигрыша с увеломлением об этом
 def check_winnig():
     for el in game_area_data:
         if not el:
             return
 
+    show_all_mines()
     messagebox.showinfo('Уведомление', 'Победа!')
     restart_game()
 
+# Показывает все мины на игровом поле после победы или поражения
+def show_all_mines():
+    for i in range(len(game_area_data)):
+        buttons_list[i].config(text=game_area_data[i])
 
+# Функция в который описывается проигрыш после нажатия на 'мину'
 def on_cell_click(number):
     if game_area_data[number] == '☠':
+        show_all_mines()
         messagebox.showinfo('Уведомление', 'Вы проиграли!')
         restart_game()
         return
 
-    print(buttons_list)
-    game_area_data[number] = '✓'
-    buttons_list[number].config(state='disabled', text='✓')
+    count = area_data.get_count_near_mines(game_area_data, game_area_size, number)
+    game_area_data[number] = count
+    buttons_list[number].config(state='disabled', text=count)
     check_winnig()
 
 def start_game(size):
     global game_area_data, restart_button
 
     for i in range(size ** 2):
-        button = tk.Button(root, width=4, height=2, text=game_area_data[i], command=lambda number=i: on_cell_click(number))
+        button = tk.Button(root, width=4, height=2, command=lambda number=i: on_cell_click(number))
         buttons_list.append(button)
 
     for g in range(len(buttons_list)):
@@ -97,8 +110,6 @@ def start():
 root.title('Сапёр')
 root.minsize(480, 360)
 root.resizable(width=True, height=True)
-
-
 
 start()
 root.mainloop()
